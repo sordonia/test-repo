e4f22d167fe378ba70f812958597f1b997758f10
Alessandro Sordoni <alsordon@microsoft.com>
2018-03-07 19:14:40 +0000

    test job

--- test_job.py
+++ test_job.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser("")
 parser.add_argument("--data_dir", type=str)
 parser.add_argument("--model_dir", type=str)
 parser.add_argument("--log_dir", type=str)
+parser.add_argument("--p1", type=str)
 args = parser.parse_args()
 
 print(parser.model_dir)
